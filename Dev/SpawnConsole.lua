3f96093e37ec627a8b133cc315b55d11c274a2ea
lobotomyx <lbatvt@gmail.com>
2026-04-07 14:57:51 -0400

    Update SpawnConsole.lua

--- Dev/SpawnConsole.lua
+++ Dev/SpawnConsole.lua
@@ -1,13 +1,14 @@
-
+local pc 
 local console_spawned = false
 local function spawn_console()
+    pc = pc or api:get_player_controller(0)
     if pc then
         local pc_player_ref = pc.Player
             if not pc_player_ref then
                 local props = pc:get_class():get_child_properties()
                 while props ~= nil do
                     -- probably only necessary because MHUR devs want to be different and named their player ref PLAYER
-                    if props and props.get_fname and props:get_fname():to_string():equals("player", true) then
+                    if props and props.get_fname and props:get_fname():to_string():lower() == "player" then
                         pc_player_ref = pc[props:get_fname():to_string()]
                         if pc_player_ref ~= nil then break end
                     end
@@ -17,13 +18,13 @@ local function spawn_console()
             if pc_player_ref then
                 _viewport = pc_player_ref.ViewportClient
             else
-                _viewport = ("GameViewportClient"):get_first_object_matching(false)
+                _viewport = uevr.api:find_uobject("Class /Script/Engine.GameViewportClient"):get_first_object_matching(false)
             end
 
             if _viewport and _viewport.ViewportConsole == nil then
-                   local console_class = find_fast("Console")
+                   local console_class = uevr.api:find_uobject("Class /Script/Engine.Console")
 
-             local temp = api:spawn_object(console_class, _viewport)
+             local temp = uevr.api:spawn_object(console_class, _viewport)
                _viewport.ViewportConsole = temp
                if _viewport.ViewportConsole ~= nil then
                     console_spawned = true
@@ -31,7 +32,5 @@ local function spawn_console()
                end
             end
         end
-
-
 end
 spawn_console()
